10c92eacf6f3a6edb54f8a3d76bfa1b8d12edcdf
Spencer Williams <s@spencerenglish.com>
2013-09-13 17:18:49 -0700

    codepen dump

--- README.md
+++ README.md
@@ -2,3 +2,134 @@ hoverpie
 ========
 
 A simple pie chart builder based on EaselJS
+
+Dependencies
+--------
+
+- [jQuery](http://jquery.com/)
+- [EaselJS](http://www.createjs.com/#!/EaselJS)
+
+Usage
+--------
+
+###HTML
+
+    <canvas id="myCanvas" width="350" height="350"></canvas>
+
+###javascript
+
+    var data = [
+      {
+        percentage : 0.2,
+        color : "#c05555",
+        label : "red"
+      },{
+        percentage : 0.2,
+        color : "#ce7b2b",
+        label : "orange"
+      },{
+        percentage : 0.2,
+        color : "#dba000",
+        label : "yellow"
+      },{
+        percentage : 0.2,
+        color : "#6e8c5f",
+        label : "green"
+      },{
+        percentage : 0.2,
+        color : "#0078bd",
+        label : "blue"
+      }];
+    var canvas = $('#myCanvas');
+    HoverPie.make(canvas, data, {});
+    
+The third argument in `HoverPie.make(...)` refers to a `config` parameter. Acceptable config options:
+
+<table>
+  <thead>
+    <tr>
+      <th>option name</th>
+      <th>type</th>
+      <th>default</th>
+      <th>description</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><code>canvasPadding</code></td>
+      <td>integer</td>
+      <td>25</td>
+      <td>pixels between unhovered pie and canvas edge</td>
+    </tr>
+    <tr>
+      <td><code>hoverScaleX</code></td>
+      <td>float</td>
+      <td>1.1</td>
+      <td>scale for the x axis of a hovered pie sector</td>
+    </tr>
+    <tr>
+      <td><code>hoverScaleY</code></td>
+      <td>float</td>
+      <td>1.1</td>
+      <td>scale for the y axis of a hovered pie sector</td>
+    </tr>
+    <tr>
+      <td><code>labelColor</code></td>
+      <td>CSS color</td>
+      <td>rgba(255,255,255,0.5)</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td><code>labelHoverColor</code></td>
+      <td>CSS color</td>
+      <td>rgba(255,255,255,1)</td>
+      <td></td>
+    <tr>
+      <td><code>labelRadiusFactor</code></td>
+      <td>float</td>
+      <td>0.66</td>
+      <td>This factor is multiplied with the pie's radius to determine where the labels are centered.</td>
+    </tr>
+    <tr>
+      <td><code>labelFontFamily</code></td>
+      <td>CSS font family</td>
+      <td>Arial</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td><code>labelFontWeight</code></td>
+      <td>CSS font weight</td>
+      <td>normal</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td><code>labelFontSize</code></td>
+      <td>pixel number</td>
+      <td>16</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td><code>sectorFillColor</code></td>
+      <td>CSS color</td>
+      <td>#666</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td><code>sectorStrokeColor</code></td>
+      <td>CSS color</td>
+      <td>#fff</td>
+      <td></td>
+    </tr>
+    <tr>
+      <td><code>sectorStrokeWidth</code></td>
+      <td>float</td>
+      <td>2</td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+
+License
+-------
+
+This library is available under the MIT License. See LICENSE for more information.
---
+++ hoverpie.js
@@ -0,0 +1,165 @@
+var HoverPie = {};
+
+HoverPie.config = {
+  canvasPadding : 25,
+  hoverScaleX : 1.1,
+  hoverScaleY : 1.1,
+  labelRadiusFactor : 0.66,
+  labelColor : "rgba(255,255,255,0.5)",
+  labelHoverColor : "rgba(255,255,255,1)",
+  labelFontFamily : "Arial",
+  labelFontWeight : "normal",
+  labelFontSize : 16,
+  sectorFillColor : "#666",
+  sectorStrokeColor : "#fff",
+  sectorStrokeWidth : 2,
+};
+HoverPie.make = (function($canvas, data, config){
+  
+  config = $.extend({}, HoverPie.config, config);
+  
+  var percent2radians = (function(percent) { return percent*Math.PI*2; });
+  
+  var ctx = $canvas[0].getContext("2d");
+  var oX = ctx.canvas.width/2;
+  var oY = ctx.canvas.height/2;
+  var r = Math.min(oX,oY) - config.canvasPadding;
+  var stage = new createjs.Stage("myCanvas");
+  stage.enableMouseOver(20);
+  
+  var cumulativeAngle = 1.5*Math.PI;
+  
+  for (var i=0; i<data.length; i++) {
+    
+    var sector = new createjs.Shape();
+    var container = new createjs.Container();
+    container.name = container.id;
+    
+    // Draw the arc
+    var sectorFillColor = data[i].fillColor || config.sectorFillColor;
+    var sectorStrokeColor = data[i].strokeColor || config.sectorStrokeColor;
+    sector.graphics.moveTo(oX,oY).beginFill(sectorFillColor).setStrokeStyle(config.sectorStrokeWidth).beginStroke(sectorStrokeColor);
+    
+    var sectorAngle = percent2radians(data[i].percentage);
+    sector.graphics.arc(oX,oY,r,cumulativeAngle,cumulativeAngle+sectorAngle);
+    
+    sector.graphics.closePath();
+    
+    container.addChild(sector);
+    
+    // Draw the label
+    if (data[i].label) {
+      
+      // One for unhovered sectors
+      var font = config.labelFontWeight+" "+config.labelFontSize+"px "+config.labelFontFamily;
+      var unhoverLabel = new createjs.Text(data[i].label,font,config.labelColor);
+      unhoverLabel.textAlign = "center";
+      unhoverLabel.textBaseline = "bottom";
+      
+      // The label is to be placed such that the center of its baseline
+      // is tangent to a circle of radius r*config.labelRadiusFactor
+      // and a line drawn along the center of the sector
+      var unhoverLabelRadius = r*config.labelRadiusFactor;
+      var unhoverLabelAngle = cumulativeAngle + sectorAngle/2.0;
+      var labelX = oX + unhoverLabelRadius * Math.cos(unhoverLabelAngle);
+      var labelY = oY + unhoverLabelRadius * Math.sin(unhoverLabelAngle);
+      unhoverLabel.x = labelX;
+      unhoverLabel.y = labelY;
+      unhoverLabel.name = "label";
+      
+      // and one for hovered sectors
+      var hoverLabel = new createjs.Text(data[i].label,font,config.labelHoverColor);
+      hoverLabel.textAlign = "center";
+      hoverLabel.textBaseline = "bottom";
+      
+      hoverLabel.x = labelX;
+      hoverLabel.y = labelY;
+      hoverLabel.name = "hoverLabel";
+      hoverLabel.visible = false;
+      
+      container.addChild(unhoverLabel);
+      container.addChild(hoverLabel);
+    }
+    
+    // reposition scale origin and draw origin
+    container.regX = oX;
+    container.regY = oY;
+    container.x = oX;
+    container.y = oY;
+    
+    cumulativeAngle+=sectorAngle;
+    stage.addChild(container);
+    stage.update();
+  } // percentages loop
+  
+  // This array tracks the currently-hovered pie sectors.
+  // if it is empty, there are no sectors hovered.
+  var hovers = [];
+  
+  // This function is to be called with a list of stage IDs
+  // it will revert any currently-hovered elements to their
+  // original style, and apply hover style to the new set.
+  var hover = (function(ids){
+    //console.log(ids,stage.children);
+    
+    // any ids in hovers that aren't in ids need to be unhovered
+    var toUnhover = [];
+    for (var i=0; i<hovers.length; i++) {
+      if (ids.indexOf(hovers[i]) == -1) {
+        // didn't find hover[i] in ids, so add to toUnhover
+        toUnhover.push(hovers[i]);
+      }
+    }
+    for (var i=0; i<toUnhover.length; i++) {
+      var child = stage.getChildByName(toUnhover[i]);
+      child.scaleX = 1;
+      child.scaleY = 1;
+      child.getChildByName("label").visible = true;
+      child.getChildByName("hoverLabel").visible = false;
+    }
+    
+    // and ids in ids that aren't in hovers need to be hovered
+    var toHover = [];
+    for (var i=0; i<ids.length; i++) {
+      if (hovers.indexOf(ids[i]) == -1) {
+        // didn't find ids[i] in hovers, so add to toHover
+        toHover.push(ids[i]);
+      }
+    }
+    for (var i=0; i<toHover.length; i++) {
+      var child = stage.getChildByName(toHover[i]);
+      child.scaleX = config.hoverScaleX;
+      child.scaleY = config.hoverScaleY;
+      child.getChildByName("label").visible = false;
+      child.getChildByName("hoverLabel").visible = true;
+    }
+    
+    hovers = ids;
+    stage.update();
+  });
+  
+  // This binding fires hover() when the mouse hovers over a
+  // new set of pie sectors.
+  // We can't use an addEventListener on each sector because
+  // the hit mask on those shapes is the size of the entire
+  // pie, not the individual sector.
+  // I have not yet tried using canvas clip(). Maybe that's
+  // a better solution? Unsure.
+  $canvas.mousemove(function(e){
+    var objs = stage.getObjectsUnderPoint(e.clientX,e.clientY);
+    var ids = $.map(objs,function(e){ return e.parent.id; });
+    
+    // call hover() if ids does not match current hovers
+    if (ids.length != hovers.length) {
+      hover(ids);
+      return;
+    }
+    for (var i=0; i<hovers.length; i++) {
+      if (ids[i] != hovers[i]) {
+        hover(ids);
+        return;
+      }
+    }
+  });
+});
+
